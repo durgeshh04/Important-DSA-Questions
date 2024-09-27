d78517a61d2cd021a9998a1d7c50c5392dd873cd
Durgesh Chaudhari <133797261+durgeshh04@users.noreply.github.com>
2024-09-27 11:37:44 +0530

    update code

--- Day-27-09-24/RemoveDuplicates.js
+++ Day-27-09-24/RemoveDuplicates.js
@@ -1,4 +1,4 @@
-// Topic: Remove Duplicates Elements from an sorted array;
+// Topic: Remove Duplicates Elements from an sorted array-I;
 /*
 Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. The relative order of the elements should be kept the same. Then return the number of unique elements in nums.
 
