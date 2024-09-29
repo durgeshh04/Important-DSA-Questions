55a912a836ace392a6697acf472efeecf74b6e4d
Durgesh Chaudhari <133797261+durgeshh04@users.noreply.github.com>
2024-09-29 23:46:12 +0530

    update solution

--- Day-29-09-24/Stock_I.js
+++ Day-29-09-24/Stock_I.js
@@ -25,3 +25,37 @@ Explanation: In this case, no transactions are done and the max profit = 0.
 
 
 */
+
+
+
+// Solution:
+
+var maxProfit = function(prices) {
+    let minPrice = Infinity;
+    let maxProfit = 0;
+    for (let i = 0; i < prices.length; i++) {
+        if (prices[i] < minPrice) {
+            minPrice = prices[i];
+        } else if (prices[i] - minPrice > maxProfit) {
+            maxProfit = prices[i] - minPrice;
+        }
+    }
+
+    return maxProfit;
+};
+
+const result = maxProfit([7,1,5,3,6,4]);
+console.log(result);
+
+
+
+
+
+
+
+
+
+
+
+
+
