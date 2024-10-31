85bcab6d15256503e9a2da41ebe1e19115112718
Durgesh Chaudhari <133797261+durgeshh04@users.noreply.github.com>
2024-10-31 23:26:40 +0530

    problem solved

---
+++ 238. Product of Array Except Self.js
@@ -0,0 +1,20 @@
+var productExceptSelf = function (nums) {
+  let resultArray = []
+  let leftProduct = 1;
+  let rightProduct = 1;
+
+  for (let i = 0; i < nums.length; i++) {
+    resultArray[i] = leftProduct;
+    leftProduct *= nums[i];
+  }
+
+  for(let i = nums.length-1; i >= 0; i--){
+    resultArray[i] *= rightProduct;
+    rightProduct *= nums[i];
+  }
+
+  return resultArray;
+};
+
+console.log(productExceptSelf([1, 2, 3, 4]));
+// console.log(productExceptSelf([-1,1,0,-3,3]));
