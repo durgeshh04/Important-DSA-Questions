45bf4748d14d5300690d69ba3e75e101d6da8a99
Durgesh Chaudhari <133797261+durgeshh04@users.noreply.github.com>
2024-11-13 13:23:39 +0530

    invertedPyramid-pattern

---
+++ Pattern-Problems/InvertedPyramid.js
@@ -0,0 +1,13 @@
+function invertedPyramid(n) {
+  for (let i = n; i >= 1; i--) {
+    for(let j = 1; j <= n-i; j++){
+        process.stdout.write("  ");
+    }
+    for(let k = 1; k < i*2; k++){
+        process.stdout.write("* ");
+    }
+    console.log();
+  }
+}
+
+invertedPyramid(5);
