402e86ff0674fbc86d729c1531b7639d6c5f7fb1
Durgesh Chaudhari <133797261+durgeshh04@users.noreply.github.com>
2024-11-13 12:48:55 +0530

    L-pattern

---
+++ Pattern-Problems/LPattern.js
@@ -0,0 +1,34 @@
+/*
+
+*         
+*
+*         
+*         
+* * * * *
+
+*/
+
+
+
+function LPattern(n) {
+    // for(let i = 1; i <= n; i++){
+    //     for(let j = 1; j <= n; j++){
+    //         if(i === n || j === 1){
+    //             process.stdout.write("* ");
+    //         } else{
+    //             process.stdout.write("  ");
+    //         }
+    //     }
+    //     console.log();
+    // }
+
+    // for (let i = 1; i < n*2; i++) {
+    //     if(i < n){
+    //         console.log("* ")
+    //     } else{
+    //         process.stdout.write("* ");
+    //     }
+    // }
+}
+
+LPattern(5);
