d8c481f33ff11db6bf5751315618d00263533edd
Durgesh Chaudhari <133797261+durgeshh04@users.noreply.github.com>
2024-11-13 11:55:39 +0530

    code update

--- Pattern-Problems/pattern-5.js
+++ Pattern-Problems/pattern-5.js
@@ -13,11 +13,10 @@
 function patternFive(n) {
   for (let i = n; i > 0; i--) {
     for (let j = 1; j <= i; j++) {
-        process.stdout.write("* ");
-        // process.stdout.write(j.toString() + " ");
+      process.stdout.write("* ");
+      // process.stdout.write(j.toString() + " ");
     }
     console.log();
-    
   }
 }
 
