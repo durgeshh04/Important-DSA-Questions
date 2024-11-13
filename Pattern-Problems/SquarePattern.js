cd89800bb5cb97b7b9b4d78941b5505d2e9a111f
Durgesh Chaudhari <133797261+durgeshh04@users.noreply.github.com>
2024-11-13 12:40:04 +0530

    square-pattern

---
+++ Pattern-Problems/SquarePattern.js
@@ -0,0 +1,29 @@
+/*
+
+* * * * * 
+*       *
+*       *
+*       * 
+* * * * *
+
+
+*/
+
+
+
+
+
+function squarePattern(n){
+    for(let i = 1; i <= n; i++){
+        for(let j = 1; j <= n; j++){
+            if(i === 1 || i === n || j === 1 || j === n){
+                process.stdout.write("* ");
+            } else{
+                process.stdout.write("  ");
+            }
+        }
+        console.log();
+    }
+}
+
+squarePattern(5);
