926ce3403912c25d51ee807be04bdf9b10c813c8
Durgesh Chaudhari <133797261+durgeshh04@users.noreply.github.com>
2025-01-31 03:40:32 +0530

    problem solved

--- Binary-Search/153.find-minimum-in-rotated-sorted-array.js
+++ Binary-Search/153.find-minimum-in-rotated-sorted-array.js
@@ -9,20 +9,29 @@
  * @param {number[]} nums
  * @return {number}
  */
-var findMin = function(nums) {
-    let left = 0;
+var findMin = function (nums) {
+    // let min = Infinity;
+    // for (let i = 0; i < nums.length; i++) {
+    //     if (nums[i] < min) {
+    //         min = nums[i];
+    //     }
+    // }
+    // return min;
+
+    let left = 0; 
     let right = nums.length - 1;
-    
-    while(left < right) {
+
+    while(left < right){
         let mid = Math.floor((left + right) / 2);
-        
-        if(nums[mid] > nums[right]) {
+        if(nums[mid] > nums[right]){
             left = mid + 1;
-        } else {
+        } else{
             right = mid;
         }
     }
+
     return nums[left];
+
 };
 // @lc code=end
 
