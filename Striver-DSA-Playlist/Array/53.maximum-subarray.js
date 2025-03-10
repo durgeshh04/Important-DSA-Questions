be1aa8c07b96d2ccf2b7175b1fa36fa4e5b5147f
Durgesh Chaudhari <133797261+durgeshh04@users.noreply.github.com>
2025-03-11 00:04:21 +0530

    problem 53 solved with the optimised approach

--- Striver-DSA-Playlist/Array/53.maximum-subarray.js
+++ Striver-DSA-Playlist/Array/53.maximum-subarray.js
@@ -10,12 +10,13 @@
  * @return {number}
  */
 var maxSubArray = function (nums) {
-    let maxSum = 0;
-    let currSum = 0;
+    let maxSum = nums[0];
+    let currSum = maxSum;
     for (let i = 1; i < nums.length; i++) {
         if (currSum < 0) {
             currSum = 0;
         }
+        currSum += nums[i];
         maxSum = Math.max(maxSum, currSum);
     }
     return maxSum;
