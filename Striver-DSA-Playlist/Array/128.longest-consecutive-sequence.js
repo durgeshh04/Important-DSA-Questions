8817999f4f2c33c7b8d0babfb59f7358ea441810
Durgesh Chaudhari <133797261+durgeshh04@users.noreply.github.com>
2025-03-18 01:25:47 +0530

    128: problem solved

---
+++ Striver-DSA-Playlist/Array/128.longest-consecutive-sequence.js
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode id=128 lang=javascript
+ *
+ * [128] Longest Consecutive Sequence
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var longestConsecutive = function (nums) {
+    let longest = 0;
+    let numSet = new Set(nums);
+    for (let num of numSet) {
+        if (!numSet.has(num - 1)) {
+            let length = 1;
+            while (numSet.has(num + length)) {
+                length++;
+            }
+            longest = Math.max(longest, length);
+        }
+    }
+    return longest;
+}
+// @lc code=end
+
