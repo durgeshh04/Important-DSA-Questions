43a5e706ce18d8a50d4d56972d7d579b3a2040ee
Durgesh Chaudhari <133797261+durgeshh04@users.noreply.github.com>
2025-03-19 23:24:15 +0530

    problem 66: plus one solved

---
+++ Array/66.plus-one.js
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode id=66 lang=javascript
+ *
+ * [66] Plus One
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+var plusOne = function (digits) {
+    for (let i = digits.length - 1; i >= 0; i--) {
+        if (digits[i] + 1 != 10) {
+            digits[i] += 1;
+            return digits;
+        }
+
+        digits[i] = 0;
+        if (i == 0) {
+            digits.unshift(1);
+            return digits;
+        }
+    }
+};
+// @lc code=end
+
