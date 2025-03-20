fcc8eb7e65a18f1aede76683643f03244f0f070a
Durgesh Chaudhari <133797261+durgeshh04@users.noreply.github.com>
2025-03-20 23:54:33 +0530

    problem commit

---
+++ Array/315.count-of-smaller-numbers-after-self.js
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode id=315 lang=javascript
+ *
+ * [315] Count of Smaller Numbers After Self
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var countSmaller = function(nums) {
+    
+};
+// @lc code=end
+
---
+++ Leetcode-Problems/3151.special-array-i.js
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode id=3151 lang=javascript
+ *
+ * [3151] Special Array I
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var isArraySpecial = function(nums) {
+    
+};
+// @lc code=end
+
