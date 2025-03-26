82533b8e0fdb5096647cb18db66b1254891a44a1
Durgesh Chaudhari <133797261+durgeshh04@users.noreply.github.com>
2025-03-27 01:34:02 +0530

    optimal solution for problem 242

--- Neetcode-150/242/242.valid-anagram.js
+++ Neetcode-150/242/242.valid-anagram.js
@@ -10,25 +10,48 @@
  * @param {string} t
  * @return {boolean}
  */
-var isAnagram = function(s, t) {
+var isAnagram = function (s, t) {
 
-    if(s.length != t.length){
+    if (s.length != t.length) {
         return false;
     }
 
-    let map = new Map();
-    for(let char of s){
-        map.set(char, (map.get(char) || 0) + 1);
+    // Create array of 26 zeros to track character counts (a-z)
+    let charCounts = new Array(26).fill(0);
+    
+    // For each character position in both strings:
+    // Increment count for chars in s
+    // Decrement count for chars in t
+    // We subtract 97 to map 'a' to index 0, 'b' to 1, etc.
+    for(let i = 0; i < s.length; i++){
+        charCounts[s.charCodeAt(i) - 97]++;
+        charCounts[t.charCodeAt(i) - 97]--;
     }
 
-    for(let char of t){
-        if(!map.has(char) || map.get(char) == 0){
+    // Check if all counts are 0
+    // If any count is not 0, strings are not anagrams
+    for(let count of charCounts){
+        if(count !== 0){
             return false;
         }
-        map.set(char, (map.get(char) - 1));
     }
 
+    // If we get here, all counts are 0 so strings are anagrams
     return true;
+
+    // let map = new Map();
+    // for(let char of s){
+    //     map.set(char, (map.get(char) || 0) + 1);
+    // }
+
+    // for(let char of t){
+    //     if(!map.has(char) || map.get(char) == 0){
+    //         return false;
+    //     }
+    //     map.set(char, (map.get(char) - 1));
+    // }
+
+    // return true;
 
 };
 // @lc code=end
