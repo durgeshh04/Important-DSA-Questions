8b4ade798d1ec3a50e4a0c47e6dd1ede36109b75
Durgesh Chaudhari <133797261+durgeshh04@users.noreply.github.com>
2025-03-28 00:32:44 +0530

    solved 1: Two Sum problem

--- Neetcode-150/1/1.two-sum.js
+++ Neetcode-150/1/1.two-sum.js
@@ -10,8 +10,15 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function(nums, target) {
-    
+var twoSum = function (nums, target) {
+    let map = new Map();
+    for (let i = 0; i < nums.length; i++) {
+        let pairValue = target - nums[i];
+        if (map.has(pairValue)) {
+            return [map.get(pairValue), i];
+        }
+        map.set(nums[i], i);
+    }
 };
 // @lc code=end
 
