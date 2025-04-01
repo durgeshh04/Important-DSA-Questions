36261c681f43e14e0a5eedb4561eb6f6151bf911
Durgesh Chaudhari <133797261+durgeshh04@users.noreply.github.com>
2025-04-02 01:14:22 +0530

    problem 128 longest consecutive sequence solved

--- Neetcode-150/128/128.longest-consecutive-sequence.js
+++ Neetcode-150/128/128.longest-consecutive-sequence.js
@@ -9,8 +9,24 @@
  * @param {number[]} nums
  * @return {number}
  */
-var longestConsecutive = function(nums) {
-    
+var longestConsecutive = function (nums) {
+    if (nums.length == 0) return 0;
+
+    let longest = 0;
+    let numSet = new Set(nums);
+    for (let num of numSet) {
+        if (!numSet.has(num - 1)) {
+            let currentNum = num;
+            let currentStreak = 1;
+
+            while (numSet.has(currentNum + 1)) {
+                currentNum += 1;
+                currentStreak += 1;
+            }
+            longest = Math.max(longest, currentStreak);
+        }
+    }
+    return longest;
 };
 // @lc code=end
 
