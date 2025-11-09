06501e6735fd4cef2c2210115066468496915eaf
durgeshh04 <durgeshchaudhari199@gmail.com>
2025-11-10 00:29:16 +0530

    second largest number problem solved

---
+++ Namste-DSA/03: Arrays - Easy & Medium/01.second-largest-number.ts
@@ -0,0 +1,23 @@
+const secondLargetNumber = (arr: number[]): number | string => {
+  let firstLargest: number = -Infinity;
+  let secondLargest: number = -Infinity;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (firstLargest < arr[i]) {
+      secondLargest = firstLargest;
+      firstLargest = arr[i];
+    } else if (secondLargest < firstLargest && secondLargest < arr[i]) {
+      secondLargest = arr[i];
+    }
+  }
+
+  // Depends on what interviewer wants for this test case
+  if (secondLargest == firstLargest) {
+    return "print whatever as interviewer wants";
+  }
+  return secondLargest;
+};
+
+console.log(secondLargetNumber([1, 5, 9, 3]));
+console.log(secondLargetNumber([11, 3, 2, 8]));
+console.log(secondLargetNumber([1, 1, 1, 1]));
